2e6d88aaa37a87d1e49d43ce36ba8fd25a577d0f
bcatronnd <bcatron@nd.edu>
2021-09-12 16:06:36 -0400

    Introduction Matlab Upload

---
+++ matlab/01_introduction/strehl_ratio.m
@@ -0,0 +1,17 @@
+close all; clc; clearvars;
+
+lambda = (1:0.1:10)*1e-6;
+OPD = 3.821e-7;
+SR = exp(-1*(2*pi*OPD./lambda).^2);
+
+%%%%% Plot
+f1 = figure;
+plot(lambda*1e6,SR,'k','linewidth',1.25); grid on;
+xlabel('$\lambda$ ($\mu m$)','interpreter','latex');
+ylabel('Strehl Ratio, SR ($I/I_0$)','interpreter','latex');
+xlim([0 10]);
+f1.Children(1).Layer = 'top';
+f1.Children(1).TickLabelInterpreter = 'latex';
+f1.Units = 'inches';
+f1.Position = [1 1 5 3];
+saveas(f1,'strehl_ratio.eps','epsc')
